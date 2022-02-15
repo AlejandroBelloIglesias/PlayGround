4e64b36ca3da6c7c5b5cd1a65f8b05d163e8e64e
Alejandro Bello <alejandro.bello.iglesias@gmail.com>
2022-02-15 21:43:53 +0100

    RetoHP4 avances

--- RetoHP4/main.py
+++ RetoHP4/main.py
@@ -1,40 +1,35 @@
-# Reads a csv document
 import csv
-from Transaction import Transaction
 
-# Writes into mysql database
-import mysql.connector
-from mysql.connector import Error
+# Rows of the transactions are: 
+    # step, type, amount, nameOrig, oldbalanceOrg, newbalanceOrig, nameDest, oldbalanceDest, newbalanceDest, isFraud, isFlaggedFraud
+def filterType(row):
+    if row[1] == 'CASH_OUT':
+        return True
+    else:
+        return False
 
+def filterFraud(row):
+    if row[9] == '1':
+        return True
+    else:
+        return False
+
+# Opening .csv
 with open('../../fraud_log.csv', 'r') as csv_file:
 
     csv_reader = csv.reader(csv_file)
-    transactions = []
 
     row = next(csv_reader)
-    print(row) # Print the header
-    
+
+    counter = 0
     for row in csv_reader: # Read the csv file row by row
-        row = next(csv_reader)
-        if (row[9] == '1'):  # If isFraud
-            if (row[10] == '1'):
-                transactions.append(
-                    Transaction(
-                        int(row[0]), # step
-                        row[1], # type
-                        float(row[2]), # amount 
-                        row[3], # nameOrig
-                        float(row[4]), # oldbalanceOrg
-                        float(row[5]), # newbalanceOrig
-                        row[6], # nameDest
-                        float(row[7]), # oldbalanceDest
-                        float(row[8]), # newbalanceDest
-                        bool(row[9]), # isFraud
-                        bool(row[10]) # isFlaggedFraud
-                    )
-                )
-                print(row)
+        if filterType(row) and filterFraud(row):
+            # Saves the row in a new csv file
+            with open('../../fraud_log_filtered.csv', 'a', newline='') as csv_file:
+                csv_writer = csv.writer(csv_file)
+                csv_writer.writerow(row)
+                counter += 1
         
-    print(transactions.toString())
-        
+print('Number of rows: ', counter)
+
 
