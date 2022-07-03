72c400f0b038b3321ee72b5904d4da11c3a03b95
Alejandro Bello Iglesias <alejandro.bello.iglesias@gmail.com>
2022-07-03 22:42:19 +0200

    header and adding kart items

---
+++ svelte/shop-test/src/lib/Header.svelte
@@ -0,0 +1,39 @@
+<script>
+  let kartEnabled = false;
+  import { kart_items } from "../store.js"
+</script>
+
+<nav class="flex items-center justify-between flex-wrap p-6 shadow-md">
+    
+  <div class="flex items-center flex-shrink-0 mr-6">
+    <span class="font-semibold text-3xl">ShopKartn't</span>
+  </div>
+
+  <button 
+    class="bg-green-700 px-3 py-2 rounded-lg text-white text-sm"
+    on:click={() => kartEnabled = !kartEnabled}
+    >
+    Kart
+  </button>
+
+  {#if kartEnabled}
+  <div class="absolute right-5 top-20 w-56 rounded-sm shadow-lg bg-white ring-1 ring-black ring-opacity-10">
+    {#if $kart_items.length}
+    {#each $kart_items as item}
+    <div class="flex items-center justify-between pl-2 pr-5 py-2 gap-4">
+        <img class="w-13 h-10 rounded-full" src="{item.img}" alt="{item.name}">
+        <span>{item.name}</span>
+        <span>{item.amount}</span>
+    </div>
+    <hr/>
+    {/each}
+    {:else}
+    <div class="text-center py-3">
+      <span class="text-gray-500">Vacío</span>
+    </div>
+    {/if}
+
+  </div>
+  {/if}
+  
+</nav>
--- svelte/shop-test/src/routes/index.svelte
+++ svelte/shop-test/src/routes/index.svelte
@@ -1,2 +1,73 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { kart_items } from '../store.js'
+
+    let items = [
+        {
+            name:"Tomate",
+            img:"/assets/tomate.jpg",
+            price:1500
+        },
+        {
+            name:"Lechuga",
+            img:"/assets/lechuga.jpg",
+            price:1200
+        },
+        {
+            name:"Guisantes",
+            img:"/assets/guisantes.jpg",
+            price:1000
+        },
+    ]
+
+    function addToKart(item){
+        //If contains the item, increase the amount
+        let itemInKart = $kart_items.find(i => i.name == item.name)
+        if(itemInKart){
+            $kart_items = $kart_items.map(i => {
+                if(i.name == item.name){
+                    i.amount++
+                }
+                return i
+            })
+        }else{
+            $kart_items = [...$kart_items, {
+                name:item.name,
+                img:item.img,
+                price:item.price,
+                amount:1
+            }]
+        }
+        console.log($kart_items)
+    }
+
+</script>
+
+<div class="xl:mx-32">
+
+    <h1 class="text-2xl font-semibold my-6">
+        Shop
+    </h1>
+
+    <!-- grid -->
+    <div class="grid grid-flow-row-dense grid-cols-2 md:grid-cols-3 gap-3">
+
+        <!-- items -->
+        {#each items as item}
+        <div class="max-w-sm rounded overflow-hidden shadow-lg border p-1">
+            <img class="w-full" src={item.img} alt="Tomate">
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">{item.name}</div>
+              <p class="text-gray-700 text-base">
+                {item.price}
+              </p>
+            </div>
+            <div class="px-6 pt-4 pb-2">
+                <button class="bg-slate-800 text-white text-sm p-2 rounded-md"
+                    on:click={()=>addToKart(item)}>
+                    Add to kart
+                </button>
+            </div>
+        </div>
+        {/each}
+    </div>
+</div>
